0d57733c860fd6422d42758bc7939cdbbedac789
Markus Kellerer <markus.kellerer@maibornwolff.de>
2024-02-02 15:38:02 +0100

    Databind the model to the XAML page Orders not displayed

--- WpfSandbox/WpfSandbox/MainWindow.xaml.cs
+++ WpfSandbox/WpfSandbox/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace WpfSandbox
 {
@@ -20,9 +21,31 @@ namespace WpfSandbox
     /// </summary>
     public partial class MainWindow : Window
     {
+        NorthwindEntities context = new NorthwindEntities();
+        CollectionViewSource custViewSource;
+        CollectionViewSource ordViewSource;
+
         public MainWindow()
         {
             InitializeComponent();
+            custViewSource = ((CollectionViewSource)(FindResource("customerViewSource")));
+            ordViewSource = ((CollectionViewSource)(FindResource("customerOrdersViewSource")));
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load is an extension method on IQueryable,    
+            // defined in the System.Data.Entity namespace.   
+            // This method enumerates the results of the query,    
+            // similar to ToList but without creating a list.   
+            // When used with Linq to Entities, this method    
+            // creates entity objects and adds them to the context.   
+            context.Customers.Load();
+
+            // After the data is loaded, call the DbSet<T>.Local property    
+            // to use the DbSet<T> as a binding source.   
+            custViewSource.Source = context.Customers.Local;
         }
     }
 }
